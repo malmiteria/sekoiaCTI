e1467b0b02de4470ef7a7ade0c93ca259243af3d
martin milon <martin.milon@ensc.fr>
2021-08-31 16:13:37 +0200

    minor refacto

--- __init__.py
+++ __init__.py
@@ -3,20 +3,19 @@ import sys
 import datetime
 import certstream
 
+
+def all_cert_update_domains(message):
+    if message['message_type'] != "certificate_update":
+        return
+    yield from message['data']['leaf_cert']['all_domains']
+
 def print_callback(message, context):
     logging.debug("Message -> {}".format(message))
-
-    if message['message_type'] == "heartbeat":
-        return
-
-    if message['message_type'] == "certificate_update":
-        all_domains = message['data']['leaf_cert']['all_domains']
-
-    if len(all_domains) == 0:
-        domain = all_domains[0]
-
-        sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
-        sys.stdout.flush()
+    for domain in all_cert_update_domains(message):
+        for keyword in ['bank']:
+            if keyword in domain:
+                sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
+                sys.stdout.flush()
 
 logging.basicConfig(format='[%(levelname)s:%(name)s] %(asctime)s - %(message)s', level=logging.INFO)
 
