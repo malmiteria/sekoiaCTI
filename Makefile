006d2f1d60817dccaf4d4c0ef24613939116f2e0
martin milon <martin.milon@ensc.fr>
2021-08-31 15:53:51 +0200

    premier commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+install-virtualenv:
+	rd local.virtualenv 2>nul &
+	python3 -m virtualenv -p python3.8 local.virtualenv
+	local.virtualenv\Scripts\activate
+	local.virtualenv\Scripts\pip install setuptools pip wheel -U
+	local.virtualenv\Scripts\pip install -r requirements.txt --find-links "file://${HOME}/.pip/wheelhouse"
+
+run:
+	local.virtualenv\Scripts\python -u -m __init__.py
---
+++ __init__.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+import datetime
+import certstream
+
+def print_callback(message, context):
+    logging.debug("Message -> {}".format(message))
+
+    if message['message_type'] == "heartbeat":
+        return
+
+    if message['message_type'] == "certificate_update":
+        all_domains = message['data']['leaf_cert']['all_domains']
+
+    if len(all_domains) == 0:
+        domain = all_domains[0]
+
+        sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
+        sys.stdout.flush()
+
+logging.basicConfig(format='[%(levelname)s:%(name)s] %(asctime)s - %(message)s', level=logging.INFO)
+
+certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
+
+
+
+
+
+
+
+
+# find all new certificates with certstream
+
+# filter them all based on the if they have keywords in the list, and are not known to be safe.
+
+# return result, as a file for example.
