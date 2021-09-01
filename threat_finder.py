1546fdbc812cf761fe9b57cb2c003ee2db8b4df2
martin milon <martin.milon@ensc.fr>
2021-09-01 16:02:23 +0200

    comment removed, if name = main pattern

--- threat_finder.py
+++ threat_finder.py
@@ -27,11 +27,10 @@ def print_callback(message, context):
     for domain, keywords in all_cert_with_keyword(message):
         if domain not in known_safe_domain:
             sys.stdout.write(", ".join(keywords) + "=====" + domain + '\n')
-            #sys.stdout.write(u"{} ==== [{}] {} (SAN: {})\n".format(keyword, datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
             sys.stdout.flush()
 
-certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
-
+if __name__ == "__main__":
+    certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
 
 
 
