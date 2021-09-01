b3ce617e0cd8340e62871c9408537a725135188f
martin milon <martin.milon@ensc.fr>
2021-09-01 17:12:22 +0200

    now possible to write result in a file, will be placed in the folder 'threats', extra parameters on the command to add the possibility of chosing the file name, stil prompting output to the console, or even not writing file at all (helpful to debug)

--- threat_finder.py
+++ threat_finder.py
@@ -1,45 +1,64 @@
 import csv
 import sys
+import getopt
 import datetime
 import certstream
 from french_bank_keywords import KEYWORDS as bank_keywords
 
+class ThreatFinder:
 
-with open('top-1m.csv') as top1m:
-    reader = csv.reader(top1m, delimiter=',')
-    known_safe_domain = [domain_name for _ , domain_name in reader]
+    def __init__(self, output_filename, no_file, verbose):
+        self.output_filename = output_filename
+        self.no_file = no_file
+        self.verbose = verbose
+        with open('top-1m.csv') as top1m:
+            reader = csv.reader(top1m, delimiter=',')
+            self.known_safe_domain = [domain_name for _ , domain_name in reader]
 
-def all_cert_update_domains(message):
-    if message['message_type'] == "certificate_update":
-        yield from message['data']['leaf_cert']['all_domains']
+    def all_cert_update_domains(self, message):
+        if message['message_type'] == "certificate_update":
+            yield from message['data']['leaf_cert']['all_domains']
 
-def all_keywords_in_domain(domain):
-    for keyword in bank_keywords:
-        if keyword in domain:
-            yield keyword
+    def all_keywords_in_domain(self, domain):
+        for keyword in bank_keywords:
+            if keyword in domain:
+                yield keyword
 
-def all_cert_with_keyword(message):
-    for domain in all_cert_update_domains(message):
-        if len(keywords := list(all_keywords_in_domain(domain))):
-            yield domain, keywords
+    def all_cert_with_keywords(self, message):
+        for domain in self.all_cert_update_domains(message):
+            if len(keywords := list(self.all_keywords_in_domain(domain))):
+                yield domain, keywords
 
-def print_callback(message, context):
-    for domain, keywords in all_cert_with_keyword(message):
-        if domain not in known_safe_domain:
-            sys.stdout.write(", ".join(keywords) + "=====" + domain + '\n')
+    def threat_reports(self, domain, keywords):
+        keywords_list = ", ".join(keywords)
+        if self.verbose:
+            sys.stdout.write(keywords_list + "=====" + domain + '\n')
             sys.stdout.flush()
+        if self.no_file:
+            return
+        with open('threats/{}.csv'.format(self.output_filename), 'a') as f:
+            csv_file = csv.writer(f, delimiter=';')
+            csv_file.writerow([domain, keywords_list])
+
+    def print_callback(self, message, context):
+        for domain, keywords in self.all_cert_with_keywords(message):
+            if domain not in self.known_safe_domain:
+                self.threat_reports(domain, keywords)
+
+def parse_command(argv):
+    output_file = "identified_threats"
+    verbose = False
+    no_file = False
+    opts, args = getopt.getopt(argv, 'vnf:', ['verbose', 'no-file', 'filename'])
+    for opt, arg in opts:
+        if opt in ['-v', '--verbose']:
+            verbose = True
+        if opt in ['-n', '--no-file']:
+            no_file = True
+        if opt in ['-f', '--filename']:
+            output_file = arg
+    return output_file, no_file, verbose
 
 if __name__ == "__main__":
-    certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
-
-
-
-
-
-
-
-# find all new certificates with certstream
-
-# filter them all based on the if they have keywords in the list, and are not known to be safe.
-
-# return result, as a file for example.
+    tf = ThreatFinder(*parse_command(sys.argv[1:]))
+    certstream.listen_for_events(tf.print_callback, url='wss://certstream.calidog.io/')
