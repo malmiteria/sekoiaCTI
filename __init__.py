a85ae51473317b539fba8ed7e65d631a7559c7f8
martin milon <martin.milon@ensc.fr>
2021-09-01 15:42:10 +0200

    ajout du fichier des domaines connus, et minor refacto

--- __init__.py
+++ __init__.py
@@ -1,20 +1,30 @@
+import csv
 import sys
 import datetime
 import certstream
 from french_bank_keywords import KEYWORDS as bank_keywords
 
 
-def all_cert_update_domains(message):
-    if message['message_type'] != "certificate_update":
-        return
-    yield from message['data']['leaf_cert']['all_domains']
+with open('top-1m.csv') as top1m:
+    reader = csv.reader(top1m, delimiter=',')
+    known_safe_domain = [domain_name for _ , domain_name in reader]
 
-def print_callback(message, context):
+def all_cert_update_domains(message):
+    if message['message_type'] == "certificate_update":
+        yield from message['data']['leaf_cert']['all_domains']
+
+def all_cert_with_keyword(message):
     for domain in all_cert_update_domains(message):
         for keyword in bank_keywords:
             if keyword in domain:
-                sys.stdout.write(u"{} ==== [{}] {} (SAN: {})\n".format(keyword, datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
-                sys.stdout.flush()
+                yield keyword, domain
+
+def print_callback(message, context):
+    for keyword, domain in all_cert_with_keyword(message):
+        if domain not in known_safe_domain:
+            sys.stdout.write(keyword + "=====" + domain + '\n')
+            #sys.stdout.write(u"{} ==== [{}] {} (SAN: {})\n".format(keyword, datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
+            sys.stdout.flush()
 
 certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
 
